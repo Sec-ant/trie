f3aefc6659c00179ff3f86180fa9c246d29ba9f0
Ze-Zheng Wu <zzwu@zju.edu.cn>
2023-12-14 09:29:11 +0800

    chore: update

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,8 @@ type Node<I, V> = Map<I, Node<I, V>> &
 
 type CountNode<I, V> = Map<WildcardCountSymbol, number> & Node<I, V>;
 
-interface SeekContext {
+interface SeekContext<I, V> {
+  parentNode?: Node<I, V>;
   nodeWildcardCount: number;
   pathWildcardCount: number;
 }
@@ -45,83 +46,8 @@ export class Trie<I, V> {
       this.setSync(key, value);
     }
   }
-  #hardSeekImpl(
-    node: Node<I, V>,
-    segment: I | WildcardSegment,
-    seekContext: SeekContext,
-  ) {
-    if (!isWildcardSegment(segment)) {
-      // TODO: we should check here
-      let childNode = node.get(segment);
-      if (!childNode) {
-        childNode = new Map();
-        node.set(segment, childNode);
-      }
-      return childNode;
-    }
-    seekContext.pathWildcardCount += segment.get(wildcardSymbol) ?? 1;
-    while (node.has(wildcardSymbol)) {
-      const childNode = node.get(wildcardSymbol)!;
-      seekContext.nodeWildcardCount += childNode.get(wildcardCountSymbol) ?? 1;
-      if (seekContext.nodeWildcardCount === seekContext.pathWildcardCount) {
-        return childNode;
-      }
-      if (seekContext.nodeWildcardCount < seekContext.pathWildcardCount) {
-        node = childNode;
-        continue;
-      }
-      // TODO: we do not check here
-      const diffCount =
-        seekContext.nodeWildcardCount - seekContext.pathWildcardCount;
-      const newChildNode = new Map() as CountNode<I, V>;
-      newChildNode.set(
-        wildcardCountSymbol,
-        childNode.get(wildcardCountSymbol)! - diffCount,
-      );
-      childNode.set(wildcardCountSymbol, diffCount);
-      node.set(wildcardSymbol, newChildNode);
-      newChildNode.set(wildcardSymbol, childNode);
-      return newChildNode;
-    }
-    if (seekContext.nodeWildcardCount >= seekContext.pathWildcardCount) {
-      return node;
-    }
-    const diffCount =
-      seekContext.pathWildcardCount - seekContext.nodeWildcardCount;
-    const childNode = new Map() as CountNode<I, V>;
-    childNode.set(wildcardCountSymbol, diffCount);
-    node.set(wildcardSymbol, childNode);
-    return childNode;
-  }
-  async #hardSeek(path: AnyIterable<I | WildcardSegment>) {
-    let node = this.#root;
-    const seekContext: SeekContext = {
-      nodeWildcardCount: 0,
-      pathWildcardCount: 0,
-    };
-    for await (const segment of path) {
-      node = this.#hardSeekImpl(node, segment, seekContext);
-    }
-    if (seekContext.nodeWildcardCount === seekContext.pathWildcardCount) {
-      return node;
-    }
-    // TODO: and check here
-    if (seekContext.nodeWildcardCount > seekContext.pathWildcardCount) {
-      assumeAs<CountNode<I, V>>(node);
-      const diffCount =
-        seekContext.nodeWildcardCount - seekContext.pathWildcardCount;
-      const newChildNode = new Map() as CountNode<I, V>;
-      newChildNode.set(
-        wildcardCountSymbol,
-        node.get(wildcardCountSymbol)! - diffCount,
-      );
-      childNode.set(wildcardCountSymbol, diffCount);
-      node.set(wildcardSymbol, newChildNode);
-      newChildNode.set(wildcardSymbol, childNode);
-      return newChildNode;
-    }
-  }
   async set(path: AnyIterable<I | WildcardSegment>, value: V) {
+    const node = await hardSeek(this.#root, path);
     const exists = node.has(dataSymbol);
     node.set(dataSymbol, value);
     if (!exists) {
@@ -130,10 +56,7 @@ export class Trie<I, V> {
     return this;
   }
   setSync(path: Iterable<I | WildcardSegment>, value: V) {
-    let node = this.#root;
-    for (const segment of path) {
-      node = this.#hardSeek(node, segment);
-    }
+    const node = hardSeekSync(this.#root, path);
     const exists = node.has(dataSymbol);
     node.set(dataSymbol, value);
     if (!exists) {
@@ -145,10 +68,7 @@ export class Trie<I, V> {
     path: AnyIterable<I | WildcardSegment>,
     callback: (prev: V | undefined, exists: boolean) => V | Promise<V>,
   ) {
-    let node = this.#root;
-    for await (const segment of path) {
-      node = this.#hardSeek(node, segment);
-    }
+    const node = await hardSeek(this.#root, path);
     const exists = node.has(dataSymbol);
     node.set(dataSymbol, await callback(node.get(dataSymbol), exists));
     if (!exists) {
@@ -160,10 +80,7 @@ export class Trie<I, V> {
     path: Iterable<I | WildcardSegment>,
     callback: (prev: V | undefined, exists: boolean) => V,
   ) {
-    let node = this.#root;
-    for (const segment of path) {
-      node = this.#hardSeek(node, segment);
-    }
+    const node = hardSeekSync(this.#root, path);
     const exists = node.has(dataSymbol);
     node.set(dataSymbol, callback(node.get(dataSymbol), exists));
     if (!exists) {
@@ -171,108 +88,37 @@ export class Trie<I, V> {
     }
     return this;
   }
-  #softSeekImpl(
-    node: Node<I, V>,
-    segment: I | WildcardSegment,
-    seekContext: SeekContext,
-    stack?: [I | WildcardSegment, Node<I, V>][],
-  ) {
-    if (!isWildcardSegment(segment)) {
-      const childNode = node.get(segment);
-      if (!childNode) {
-        stack?.splice(0, stack.length);
-        return undefined;
-      }
-      stack?.unshift([segment, node]);
-      return childNode;
-    }
-    seekContext.pathWildcardCount += segment.get(wildcardSymbol) ?? 1;
-    while (
-      node.has(wildcardSymbol) &&
-      seekContext.nodeWildcardCount < seekContext.pathWildcardCount
-    ) {
-      const childNode = node.get(wildcardSymbol)!;
-      seekContext.nodeWildcardCount += childNode.get(wildcardCountSymbol) ?? 1;
-      stack?.unshift([segment, node]);
-      node = childNode;
-    }
-    return node;
-  }
-  async #softSeek(
-    path: AnyIterable<I | WildcardSegment>,
-    stack?: [I | WildcardSegment, Node<I, V>][],
-  ) {
-    let node = this.#root;
-    const seekContext: SeekContext = {
-      nodeWildcardCount: 0,
-      pathWildcardCount: 0,
-    };
-    for await (const segment of path) {
-      const childNode = this.#softSeekImpl(node, segment, seekContext, stack);
-      if (!childNode) {
-        return undefined;
-      }
-      node = childNode;
-    }
-    if (seekContext.nodeWildcardCount !== seekContext.pathWildcardCount) {
-      stack?.splice(0, stack.length);
-      return undefined;
-    }
-    return node;
-  }
-  #softSeekSync(
-    path: Iterable<I | WildcardSegment>,
-    stack?: [I | WildcardSegment, Node<I, V>][],
-  ) {
-    let node = this.#root;
-    const seekContext: SeekContext = {
-      nodeWildcardCount: 0,
-      pathWildcardCount: 0,
-    };
-    for (const segment of path) {
-      const childNode = this.#softSeekImpl(node, segment, seekContext, stack);
-      if (!childNode) {
-        return undefined;
-      }
-      node = childNode;
-    }
-    if (seekContext.nodeWildcardCount !== seekContext.pathWildcardCount) {
-      stack?.splice(0, stack.length);
-      return undefined;
-    }
-    return node;
-  }
   async has(path: AnyIterable<I | WildcardSegment>) {
-    return Boolean(await this.#softSeek(path));
+    return !!(await softSeek(this.#root, path));
   }
   hasSync(path: Iterable<I | WildcardSegment>) {
-    return Boolean(this.#softSeekSync(path));
+    return !!softSeekSync(this.#root, path);
   }
   async get(path: AnyIterable<I | WildcardSegment>) {
-    return (await this.#softSeek(path))?.get(dataSymbol);
+    return (await softSeek(this.#root, path))?.get(dataSymbol);
   }
   getSync(path: Iterable<I | WildcardSegment>) {
-    return this.#softSeekSync(path)?.get(dataSymbol);
+    return softSeekSync(this.#root, path)?.get(dataSymbol);
   }
-  // async delete(path: AnyIterable<I | WildcardSegment>) {
-  //   const stack: [I, Node<I, V>][] = [];
-  //   const node = await this.#seek(path, stack);
-  //   if (!node) {
-  //     return false;
-  //   }
-  //   node.delete(dataSymbol);
-  //   --this.#size;
-  //   if (node.size) {
-  //     return true;
-  //   }
-  //   for (const [segment, node] of stack) {
-  //     node.delete(segment);
-  //     if (node.size) {
-  //       return true;
-  //     }
-  //   }
-  //   return true;
-  // }
+  async delete(path: AnyIterable<I | WildcardSegment>) {
+    const stack: [I, Node<I, V>][] = [];
+    const node = await softSeek(this.#root, path, stack);
+    if (!node) {
+      return false;
+    }
+    node.delete(dataSymbol);
+    --this.#size;
+    if (node.size) {
+      return true;
+    }
+    for (const [segment, node] of stack) {
+      node.delete(segment);
+      if (node.size) {
+        return true;
+      }
+    }
+    return true;
+  }
   // deleteSync(path: Iterable<I | WildcardSegment>) {
   //   const stack: [I, Node<I, V>][] = [];
   //   const node = this.#seekSync(path, stack);
@@ -516,4 +362,172 @@ function isWildcardSegment(segment: unknown): segment is WildcardSegment {
 
 function isCountNode<I, V>(node: Node<I, V>): node is CountNode<I, V> {
   return (node as CountNode<I, V>).has(wildcardCountSymbol);
+}
+
+function hardSeekResolve<I, V>(
+  node: Node<I, V>,
+  seekContext: SeekContext<I, V>,
+) {
+  if (seekContext.nodeWildcardCount < seekContext.pathWildcardCount) {
+    const diffCount =
+      seekContext.pathWildcardCount - seekContext.nodeWildcardCount;
+    const nextNode = new Map() as CountNode<I, V>;
+    nextNode.set(wildcardCountSymbol, diffCount);
+    node.set(wildcardSymbol, nextNode);
+    node = nextNode;
+  } else if (seekContext.nodeWildcardCount > seekContext.pathWildcardCount) {
+    assumeAs<CountNode<I, V>>(node);
+    const diffCount =
+      seekContext.nodeWildcardCount - seekContext.pathWildcardCount;
+    const nextNode = new Map() as CountNode<I, V>;
+    nextNode.set(
+      wildcardCountSymbol,
+      node.get(wildcardCountSymbol)! - diffCount,
+    );
+    node.set(wildcardCountSymbol, diffCount);
+    seekContext.parentNode!.set(wildcardSymbol, nextNode);
+    nextNode.set(wildcardSymbol, node);
+    node = nextNode;
+  }
+  seekContext.nodeWildcardCount = 0;
+  seekContext.pathWildcardCount = 0;
+  seekContext.parentNode = undefined;
+  return node;
+}
+
+function hardSeekImpl<I, V>(
+  node: Node<I, V>,
+  segment: I | WildcardSegment,
+  seekContext: SeekContext<I, V>,
+) {
+  if (!isWildcardSegment(segment)) {
+    node = hardSeekResolve(node, seekContext);
+    let childNode = node.get(segment);
+    if (!childNode) {
+      childNode = new Map();
+      node.set(segment, childNode);
+    }
+    return childNode;
+  }
+  seekContext.pathWildcardCount += segment.get(wildcardSymbol) ?? 1;
+  while (
+    node.has(wildcardSymbol) &&
+    seekContext.nodeWildcardCount < seekContext.pathWildcardCount
+  ) {
+    const childNode = node.get(wildcardSymbol)!;
+    seekContext.nodeWildcardCount += childNode.get(wildcardCountSymbol) ?? 1;
+    seekContext.parentNode = node;
+    node = childNode;
+  }
+  return node;
+}
+
+async function hardSeek<I, V>(
+  root: Node<I, V>,
+  path: AnyIterable<I | WildcardSegment>,
+) {
+  let node = root;
+  const seekContext: SeekContext<I, V> = {
+    nodeWildcardCount: 0,
+    pathWildcardCount: 0,
+  };
+  for await (const segment of path) {
+    node = hardSeekImpl(node, segment, seekContext);
+  }
+  return hardSeekResolve(node, seekContext);
+}
+
+function hardSeekSync<I, V>(
+  root: Node<I, V>,
+  path: Iterable<I | WildcardSegment>,
+) {
+  let node = root;
+  const seekContext: SeekContext<I, V> = {
+    nodeWildcardCount: 0,
+    pathWildcardCount: 0,
+  };
+  for (const segment of path) {
+    node = hardSeekImpl(node, segment, seekContext);
+  }
+  return hardSeekResolve(node, seekContext);
+}
+
+function softSeekImpl<I, V>(
+  node: Node<I, V>,
+  segment: I | WildcardSegment,
+  seekContext: SeekContext<I, V>,
+  stack?: [I | WildcardSegment, Node<I, V>][],
+) {
+  if (!isWildcardSegment(segment)) {
+    if (seekContext.nodeWildcardCount !== seekContext.pathWildcardCount) {
+      stack?.splice(0, stack.length);
+      return undefined;
+    }
+    const childNode = node.get(segment);
+    if (!childNode) {
+      stack?.splice(0, stack.length);
+      return undefined;
+    }
+    stack?.unshift([segment, node]);
+    return childNode;
+  }
+  seekContext.pathWildcardCount += segment.get(wildcardSymbol) ?? 1;
+  while (
+    node.has(wildcardSymbol) &&
+    seekContext.nodeWildcardCount < seekContext.pathWildcardCount
+  ) {
+    const childNode = node.get(wildcardSymbol)!;
+    seekContext.nodeWildcardCount += childNode.get(wildcardCountSymbol) ?? 1;
+    stack?.unshift([segment, node]);
+    node = childNode;
+  }
+  return node;
+}
+
+async function softSeek<I, V>(
+  root: Node<I, V>,
+  path: AnyIterable<I | WildcardSegment>,
+  stack?: [I | WildcardSegment, Node<I, V>][],
+) {
+  let node = root;
+  const seekContext: SeekContext<I, V> = {
+    nodeWildcardCount: 0,
+    pathWildcardCount: 0,
+  };
+  for await (const segment of path) {
+    const childNode = softSeekImpl(node, segment, seekContext, stack);
+    if (!childNode) {
+      return undefined;
+    }
+    node = childNode;
+  }
+  if (seekContext.nodeWildcardCount !== seekContext.pathWildcardCount) {
+    stack?.splice(0, stack.length);
+    return undefined;
+  }
+  return node;
+}
+
+function softSeekSync<I, V>(
+  root: Node<I, V>,
+  path: Iterable<I | WildcardSegment>,
+  stack?: [I | WildcardSegment, Node<I, V>][],
+) {
+  let node = root;
+  const seekContext: SeekContext<I, V> = {
+    nodeWildcardCount: 0,
+    pathWildcardCount: 0,
+  };
+  for (const segment of path) {
+    const childNode = softSeekImpl(node, segment, seekContext, stack);
+    if (!childNode) {
+      return undefined;
+    }
+    node = childNode;
+  }
+  if (seekContext.nodeWildcardCount !== seekContext.pathWildcardCount) {
+    stack?.splice(0, stack.length);
+    return undefined;
+  }
+  return node;
 }
